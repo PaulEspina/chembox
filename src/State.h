4606f6309f7677699b369cfce132cdf6b011d6af
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-09-05 21:02:04 +0800

    Added State interface

---
+++ src/State.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+
+class State
+{
+public:
+	virtual ~State() = 0;
+	virtual void init() = 0;
+	virtual void tick(sf::Event&) = 0;
+	virtual void update() = 0;
+	virtual void render(sf::RenderWindow&) = 0;
+private:
+};
