1f6d3fb96664b99d887c618c16ec79cf096fb034
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-09-05 23:06:27 +0800

    Added particle system

--- src/Particle/Particle.cpp
+++ src/Particle/Particle.cpp
@@ -2,26 +2,24 @@
 
 Particle::Particle()
 {
-	label = "PARTICLE";
-	pos = sf::Vector2f(0, 0);
-	vel = sf::Vector2f(0, 0);
-	acc = sf::Vector2f(0, 0);
+	init(sf::Vector2f(0, 0), sf::Vector2f(0, 0), sf::Vector2f(0, 0));
 }
 
 Particle::Particle(sf::Vector2f pos, sf::Vector2f vel, sf::Vector2f acc)
 {
-	label = "PARTICLE";
-	this->pos = pos;
-	this->vel = vel;
-	this->acc = acc;
+	init(pos, vel, acc);
 }
 
 Particle::~Particle()
 {
 }
 
-void Particle::init()
+void Particle::init(sf::Vector2f pos, sf::Vector2f vel, sf::Vector2f acc)
 {
+	label = "PARTICLE";
+	this->pos = pos;
+	this->vel = vel;
+	this->acc = acc;
 	rect.setSize(sf::Vector2f(size, size));
 	rect.setPosition(pos);
 	rect.setFillColor(sf::Color::White);
@@ -31,10 +29,10 @@ void Particle::update()
 {
 	vel += acc;
 	pos += vel;
-	rect.setPosition(pos);
 }
 
 void Particle::render(sf::RenderWindow &window)
 {
 	window.draw(rect);
 }
+
--- src/Particle/Particle.h
+++ src/Particle/Particle.h
@@ -8,10 +8,10 @@ public:
 	Particle();
 	Particle(sf::Vector2f pos, sf::Vector2f vel = sf::Vector2f(0, 0), sf::Vector2f acc = sf::Vector2f(0, 0));
 	virtual ~Particle();
-	virtual void init();
 	virtual void update();
 	virtual void render(sf::RenderWindow &window);
 private:
+	virtual void init(sf::Vector2f pos, sf::Vector2f vel, sf::Vector2f acc);
 	static const unsigned int size = 2;
 	const char *label;
 
